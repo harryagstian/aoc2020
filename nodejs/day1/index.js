636ccaec5c0242132eae797a2b4094a71f8fbbb2
Harry Agustian <harryagustian777@yahoo.co.id>
2020-12-02 19:47:16 +0900

    Optimize logic for 2nd problem by checking the first 2 value and added performance measurement

--- nodejs/day1/index.js
+++ nodejs/day1/index.js
@@ -1,8 +1,14 @@
 const fs = require('fs')
 const readline = require('readline')
 const _ = require('lodash')
-const { Console, debug } = require('console')
-const { FORMERR } = require('dns')
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+const obs = new PerformanceObserver((items) => {
+    let { name, duration } = items.getEntries()[0]
+    console.log(`${name}: ${duration}ms`)
+    performance.clearMarks()
+})
+obs.observe({ entryTypes: ['measure'] });
 
 let inputFile = __dirname + '/input.txt'
 
@@ -16,6 +22,8 @@ let lineStream = readline.createInterface(({
     input: fs.createReadStream(inputFile),
 }))
 let stacks = [[], []]
+
+performance.mark('start')
 
 lineStream.on('line', (line) => {
     line.length === 3 ? stacks[0].push(parseInt(line)) : stacks[1].push(parseInt(line))
@@ -34,25 +42,30 @@ lineStream.on('close', () => {
     let sortedStacks = _.sortBy(stacks[0].concat(stacks[1]))
 
     // debugPrint(sortedStacks)
-    let iteration = 0
+    // let iteration = 0
     while (true) {
-        iteration++
+        // iteration++
         let workingArray = []
         let valid = []
         valid[0] = sortedStacks.pop()
 
-        workingArray = _.cloneDeep(sortedStacks)        
+        workingArray = _.cloneDeep(sortedStacks)
 
-        for(let element of workingArray) {
-            let result = workingArray.find(element2 => {
-                return valid[0] + element + element2 === 2020
-            })
+        for (let element of workingArray) {
+            let result = undefined
+
+            if (valid[0] + element < 2020) {
+                result = workingArray.find(element2 => {
+                    return valid[0] + element + element2 === 2020
+                })
+            }
+            
             // debugPrint(result, element, iteration)
-            if(result !== undefined){
+            if (result !== undefined) {
                 valid[1] = element
                 valid[2] = result
                 break;
-            }else{
+            } else {
                 valid[1] = undefined
                 valid[2] = undefined
             }
@@ -60,9 +73,10 @@ lineStream.on('close', () => {
 
         // debugPrint(iteration, valid)
 
-        if(!valid.includes(undefined)){
+        if (!valid.includes(undefined)) {
             // debugPrint(iteration, valid)
             console.log(`Result #2: ${valid[0] * valid[1] * valid[2]}`)
+            performance.measure('Start to end', 'start')
             break;
         }
     }
