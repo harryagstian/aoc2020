ec01624572520c3fb2d5464b55c9c7223e57f976
Harry Agustian <harryagustian777@yahoo.co.id>
2020-12-11 21:05:03 +0900

    Day 11 part 2 solution

--- nodejs/day11/index.js
+++ nodejs/day11/index.js
@@ -43,9 +43,11 @@ lineStream.on('line', (line) => {
 
 lineStream.on('close', () => {
     stacks.push(Array(lineLen).fill('.')) // append full floor on last line
-    
+
     let newArray = _.cloneDeep(stacks)
     let previousArray = _.cloneDeep(stacks)
+
+    // part 1
 
     let memoization = {}
     let changes = 0
@@ -102,13 +104,99 @@ lineStream.on('close', () => {
         results[0] += currentCount
     }
 
-    // part 1
     console.log(`Result #1: ${results[0]}`)
 
     // part 2
 
-    console.log(`Result #2: ${results[1]}`)
+    newArray = _.cloneDeep(stacks)
+    previousArray = _.cloneDeep(stacks)
 
+    changes = 0
+
+    memoization = {}
+
+    let findOccupiedSeats = (row, column, xDir, yDir) => {
+        let found = 0
+        // for some reason memoization is slower?
+        /* let key = `${row}%${column}%${xDir}%${yDir}`
+        
+        if (memoization[key] !== undefined) {
+            if (previousArray[memoization[key].row][memoization[key].column] === "#") {
+                found++
+            }
+
+            return found
+        } */
+
+        while (true) {
+            row += yDir
+            column += xDir
+
+            if (row < 1 || row > stacks.length - 2) break;
+            if (column < 1 || column > lineLen - 2) break;
+            if (previousArray[row][column] === "L") {
+                /* memoization[key] = {
+                    row,
+                    column
+                } */
+                break;
+            }
+            if (previousArray[row][column] === "#") {
+                /* memoization[key] = {
+                    row,
+                    column
+                } */
+                found++
+                break;
+            }
+        }
+
+        return found
+    }
+
+    let invert2 = (row, column) => {
+        let seat = previousArray[row][column]
+        if (seat !== ".") {
+            let occupied = 0
+
+            for (let i = -1; i < 2; i++) { // create key and check for occupied seats adjacent index
+                if (occupied >= 5) break;
+                for (let j = -1; j < 2; j++) {
+                    if (occupied >= 5) break;
+                    if (i === 0 && j === 0) continue;
+                    occupied += findOccupiedSeats(row, column, j, i)
+                }
+            }
+            if (seat === "L" && occupied === 0) {
+                changes++
+                seat = "#"
+                newArray[row][column] = seat
+            } else if (seat === "#" && occupied >= 5) {
+                changes++
+                seat = "L"
+                newArray[row][column] = seat
+            }
+        }
+
+    }
+
+    do {
+        changes = 0
+        for (let row = 1; row < stacks.length - 1; row++) {
+            for (let column = 1; column < lineLen - 1; column++) {
+                invert2(row, column)
+            }
+        }
+        previousArray = _.cloneDeep(newArray)
+    } while (changes > 0)
+
+    for (let i = 0; i < newArray.length; i++) {
+        let currentCount = newArray[i].filter(element => { return element === "#" }).length
+        results[1] += currentCount
+    }
+
+    // console.log(memoization.length)
+    console.log(`Result #2: ${results[1]}`)
 
     performance.mark('end')
     performance.measure('Start to end', 'start', 'end')
