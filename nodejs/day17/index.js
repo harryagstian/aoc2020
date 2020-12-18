8ac22ae92c39c244b73cd4aba4e471ac14a42981
Harry Agustian <harryagustian777@yahoo.co.id>
2020-12-18 12:46:40 +0900

    Day 17 part 2 solution, reworked logic to use map instead of arrays

--- nodejs/day17/index.js
+++ nodejs/day17/index.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const readline = require('readline')
 const _ = require('lodash')
 const { PerformanceObserver, performance } = require('perf_hooks');
-const { add } = require('lodash');
 
 const obs = new PerformanceObserver((items) => {
     let { name, duration } = items.getEntries()[0]
@@ -21,144 +20,108 @@ let lineStream = readline.createInterface(({
 }))
 
 let results = [0, 0]
-let len = 0
-let spaces = [[]]
+let activeMap
+let baseMap = new Map()
+let maxIter = 12 // if ur answer too low, set this number to higher
 
-let countNeighbor = (x, y, z) => {
-    let count = 0
-    for (let i = -1; i < 2; i++) {
-        let currX = x + i
-        if (currX < 0 || currX > spaces.length - 1) {
-            continue;
-        }
-        for (let j = -1; j < 2; j++) {
-            let currY = y + j
-            if (currY < 0 || currY > spaces[currX].length - 1) {
-                continue;
-            }
 
-            for (let k = -1; k < 2; k++) {
-                let currZ = z + k
-                // console.log(spaces, currX, currY, currZ)
-                if (currZ < 0 || currZ > spaces[currX][currY].length - 1 || (i === 0 && j === 0 && k === 0)) {
-                    continue;
-                }
-                if (spaces[currX][currY][currZ] === '#') {
-                    // console.log('found on', currX, currY, currZ, x, y, z, i, j, k)
-                    count++
-                }
-            }
-        }
-    }
-    // console.log('returned')
-    return count
+let getKey = (x, y, z, w = 0) => {
+    return `${x}.${y}.${z}.${w}`
 }
 
-let expandArray = (arr) => {
-
-    len += 2
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            arr[i][j].splice(0, 0, '.')
-            arr[i][j].push('.')
-        }
-        let t = Array(len).fill('.')
-        arr[i].splice(0, 0, t)
-        t = _.cloneDeep(t)
-        arr[i].push(t)
-    }
-
-    let temp = []
-    for (let i = 0; i < len; i++) {
-        temp.push(Array(len).fill('.'))
-    }
-
-    arr.splice(0, 0, temp)
-    temp = _.cloneDeep(temp)
-    arr.push(temp)
-
-}
+let lineCount = 0
 
 lineStream.on('line', (line) => {
-    if (len === 0) {
-        len = line.length
-        // spaces[0].push(Array(len).fill('.'))
+    for (let i = 0; i < line.length; i++) {
+        if (line[i] === "#") {
+            baseMap.set(getKey(lineCount, i, 0, 0), '#')
+        }
     }
-    // spaces[0].push(['.', ...line, '.'])
-    spaces[0].push([...line])
-    console.log(spaces)
+    lineCount++
 })
 
 
 lineStream.on('close', () => {
 
-    // console.table(spaces)
-    // JSON.stringify(spaces)
-    // expandArray(spaces)
-    // JSON.stringify(spaces)
-    // console.table(spaces)
-    // spaces[0].push(Array(len).fill('.'))
-
-
+    // part 1 - 3D
+    activeMap = _.cloneDeep(baseMap)
     for (let loop = 0; loop < 6; loop++) {
-        // let temp = []
-        // for (let i = 0; i < len; i++) {
-        //     temp.push(Array(len).fill('.'))
-        // }
-
-        // spaces.push(temp)
-        expandArray(spaces)
-        let spacesCp = _.cloneDeep(spaces)
-
-        for (let i = 0; i < spacesCp.length; i++) {
-            for (let j = 0; j < spacesCp[i].length; j++) {
-                for (let k = 0; k < spacesCp[i][j].length; k++) {
-                    
-                    // if(loop == 0 && i === 0 && j === 2&& k === 1) debugger
-                    let count = countNeighbor(i, j, k)
-                    let current = spacesCp[i][j][k]
-                    if (current === '#' && (count !== 2 && count !== 3)) {
-                        // console.log('changed # to .', i, j, k, count)
-                        spacesCp[i][j][k] = '.'
-                    } else if (current === '.' && count === 3) {
-                        // console.log('changed . to #', i, j, k, count)
-                        // console.log(current)
-                        spacesCp[i][j][k] = '#'
+        console.log(`Iteration: ${loop}, size ${activeMap.size}`)
+        // console.log(loop)
+        let activeMapCp = _.cloneDeep(activeMap)
+        for (let x = -maxIter; x < maxIter; x++) {
+            for (let y = -maxIter; y < maxIter; y++) {
+                for (let z = -maxIter; z < maxIter; z++) {
+                    let neighbor = 0
+                    for (const i of [-1, 0, 1]) {
+                        for (const j of [-1, 0, 1]) {
+                            for (const k of [-1, 0, 1]) {
+                                if (i === 0 && j === 0 && k === 0) {
+                                    continue;
+                                }
+                                if (activeMap.has(getKey(x + i, y + j, z + k))) {
+                                    neighbor++
+                                }
+                            }
+                        }
+                    }
+                    let currentKey = getKey(x, y, z)
+                    let isActive = activeMap.has(currentKey)
+                    if (isActive && (neighbor !== 2 && neighbor !== 3)) {
+                        activeMapCp.delete(currentKey)
+                    } else if (!isActive && neighbor === 3) {
+                        activeMapCp.set(currentKey, '#')
                     }
                 }
             }
         }
 
-        for (let i = 0; i < spacesCp.length; i++) {
-            // console.table(spaces[i])
-            console.table(spacesCp[i])
-        }
-        console.log(loop + 1, Array(50).fill('-').join(''))
-        // console.table(spaces)
-        // console.table(spacesCp)
-        spaces = _.cloneDeep(spacesCp)
+        activeMap = _.cloneDeep(activeMapCp)
     }
-    // console.table(spaces)
+    results[0] = activeMap.size
+    console.log(`Result #1: ${results[0]}`)
 
-    for (let i = 0; i < spaces.length; i++) {
-        for (let j = 0; j < spaces[i].length; j++) {
-            for (let k = 0; k < spaces[i][j].length; k++) {
-                if (spaces[i][j][k] === '#') {
-                    let inc = 1
+    // part 2 - 4D
+    activeMap = _.cloneDeep(baseMap)
 
-                    // if (i > 0) {
-                    //     inc = 2
-                    // }
-
-                    results[0] += inc
+    for (let loop = 0; loop < 6; loop++) {
+        console.log(`Iteration: ${loop}, size ${activeMap.size}`)
+        // console.log(loop)
+        let activeMapCp = _.cloneDeep(activeMap)
+        for (let x = -maxIter; x < maxIter; x++) {
+            for (let y = -maxIter; y < maxIter; y++) {
+                for (let z = -maxIter; z < maxIter; z++) {
+                    for (let w = -maxIter; w < maxIter; w++) {
+                        let neighbor = 0
+                        for (const i of [-1, 0, 1]) {
+                            for (const j of [-1, 0, 1]) {
+                                for (const k of [-1, 0, 1]) {
+                                    for (const l of [-1, 0, 1]) {
+                                        if (i === 0 && j === 0 && k === 0 && l === 0) {
+                                            continue;
+                                        }
+                                        if (activeMap.has(getKey(x + i, y + j, z + k, w + l))) {
+                                            neighbor++
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        let currentKey = getKey(x, y, z, w)
+                        let isActive = activeMap.has(currentKey)
+                        if (isActive && (neighbor !== 2 && neighbor !== 3)) {
+                            activeMapCp.delete(currentKey)
+                        } else if (!isActive && neighbor === 3) {
+                            activeMapCp.set(currentKey, '#')
+                        }
+                    }
                 }
             }
         }
-    }
-    // part 1
-    console.log(`Result #1: ${results[0]}`)
 
-    // part 2
+        activeMap = _.cloneDeep(activeMapCp)
+    }
+    results[1] = activeMap.size
     console.log(`Result #2: ${results[1]}`)
 
     performance.mark('end')
