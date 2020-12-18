85c9728f63fae0a7e1bdd967983fd34a19dcac84
Harry Agustian <harryagustian777@yahoo.co.id>
2020-12-18 14:05:28 +0900

    Optimize performance by using dynamic dimension size

--- nodejs/day17/index.js
+++ nodejs/day17/index.js
@@ -22,16 +22,20 @@ let lineStream = readline.createInterface(({
 let results = [0, 0]
 let activeMap
 let baseMap = new Map()
-let maxIter = 12 // if ur answer too low, set this number to higher
 
+let baseIter = [0, 0, 2, 2]
 
 let getKey = (x, y, z, w = 0) => {
     return `${x}.${y}.${z}.${w}`
 }
 
 let lineCount = 0
+let lineLen = 0
 
 lineStream.on('line', (line) => {
+    if (lineLen === 0) {
+        lineLen = line.length
+    }
     for (let i = 0; i < line.length; i++) {
         if (line[i] === "#") {
             baseMap.set(getKey(lineCount, i, 0, 0), '#')
@@ -43,15 +47,22 @@ lineStream.on('line', (line) => {
 
 lineStream.on('close', () => {
 
+    baseIter[0] = lineCount + 2
+    baseIter[1] = lineLen + 2
+
+    let maxIter = _.cloneDeep(baseIter)
+
     // part 1 - 3D
     activeMap = _.cloneDeep(baseMap)
+
     for (let loop = 0; loop < 6; loop++) {
-        console.log(`Iteration: ${loop}, size ${activeMap.size}`)
+        let maxActive = [0, 0, 0, 0]
+        console.log(`Iteration: ${loop}, size ${activeMap.size}, dimension are ${maxIter}`)
         // console.log(loop)
         let activeMapCp = _.cloneDeep(activeMap)
-        for (let x = -maxIter; x < maxIter; x++) {
-            for (let y = -maxIter; y < maxIter; y++) {
-                for (let z = -maxIter; z < maxIter; z++) {
+        for (let x = -maxIter[0]; x < maxIter[0]; x++) {
+            for (let y = -maxIter[1]; y < maxIter[1]; y++) {
+                for (let z = -maxIter[2]; z < maxIter[2]; z++) {
                     let neighbor = 0
                     for (const i of [-1, 0, 1]) {
                         for (const j of [-1, 0, 1]) {
@@ -71,27 +82,40 @@ lineStream.on('close', () => {
                         activeMapCp.delete(currentKey)
                     } else if (!isActive && neighbor === 3) {
                         activeMapCp.set(currentKey, '#')
+                        if (maxActive[0] < Math.abs(x)) {
+                            maxActive[0] = Math.abs(x)
+                        }
+                        if (maxActive[1] < Math.abs(y)) {
+                            maxActive[1] = Math.abs(y)
+                        }
+                        if (maxActive[2] < Math.abs(z)) {
+                            maxActive[2] = Math.abs(z)
+                        }
                     }
                 }
             }
         }
-
+        for (let i = 0; i < maxActive.length; i++) {
+            maxIter[i] = maxActive[i] + 2
+        }
         activeMap = _.cloneDeep(activeMapCp)
     }
     results[0] = activeMap.size
     console.log(`Result #1: ${results[0]}`)
 
     // part 2 - 4D
+    maxIter = _.cloneDeep(baseIter)
     activeMap = _.cloneDeep(baseMap)
 
     for (let loop = 0; loop < 6; loop++) {
-        console.log(`Iteration: ${loop}, size ${activeMap.size}`)
+        let maxActive = [0, 0, 0, 0]
+        console.log(`Iteration: ${loop}, size ${activeMap.size}, dimension are ${maxIter}`)
         // console.log(loop)
         let activeMapCp = _.cloneDeep(activeMap)
-        for (let x = -maxIter; x < maxIter; x++) {
-            for (let y = -maxIter; y < maxIter; y++) {
-                for (let z = -maxIter; z < maxIter; z++) {
-                    for (let w = -maxIter; w < maxIter; w++) {
+        for (let x = -maxIter[0]; x < maxIter[0]; x++) {
+            for (let y = -maxIter[1]; y < maxIter[1]; y++) {
+                for (let z = -maxIter[2]; z < maxIter[2]; z++) {
+                    for (let w = -maxIter[3]; w < maxIter[3]; w++) {
                         let neighbor = 0
                         for (const i of [-1, 0, 1]) {
                             for (const j of [-1, 0, 1]) {
@@ -108,17 +132,34 @@ lineStream.on('close', () => {
                             }
                         }
                         let currentKey = getKey(x, y, z, w)
+                        // console.log(currentKey)
                         let isActive = activeMap.has(currentKey)
                         if (isActive && (neighbor !== 2 && neighbor !== 3)) {
                             activeMapCp.delete(currentKey)
                         } else if (!isActive && neighbor === 3) {
                             activeMapCp.set(currentKey, '#')
+                            if (maxActive[0] < Math.abs(x)) {
+                                maxActive[0] = Math.abs(x)
+                            }
+                            if (maxActive[1] < Math.abs(y)) {
+                                maxActive[1] = Math.abs(y)
+                            }
+                            if (maxActive[2] < Math.abs(z)) {
+                                maxActive[2] = Math.abs(z)
+                            }
+                            if (maxActive[3] < Math.abs(w)) {
+                                maxActive[3] = Math.abs(w)
+                            }
                         }
                     }
                 }
             }
         }
-
+        // console.log(maxActive)
+        for (let i = 0; i < maxActive.length; i++) {
+            maxIter[i] = maxActive[i] + 2
+        }
+        // console.log(maxIter, maxActive)
         activeMap = _.cloneDeep(activeMapCp)
     }
     results[1] = activeMap.size
