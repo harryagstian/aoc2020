3f2f1fe6bbb4f760f6edfc576611b62a9e0d9c81
Harry Agustian <harry.agustian@sinarmas-agri.com>
2020-12-24 18:54:23 +0700

    Day 22 part 2 solution

--- nodejs/day22/index.js
+++ nodejs/day22/index.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const readline = require('readline')
 const _ = require('lodash')
 const { PerformanceObserver, performance } = require('perf_hooks');
-const { on } = require('process');
 
 const obs = new PerformanceObserver((items) => {
     let { name, duration } = items.getEntries()[0]
@@ -29,6 +28,66 @@ let baseStacks = {
     p2: []
 }
 
+let memoize = new Map()
+
+let createGameCacheKey = (p1, p2) => {
+    return `p1~${p1.join('~')}~p2~${p2.join('~')}`
+}
+
+let createMemoizeGameKey = (p1, p2, cp1, cp2) => {
+    return `p1~${p1.join('~')}~${cp1}~p2~${p2.join('~')}~${cp2}`
+}
+
+let recursiveBattle = (p1arr, p2arr, base = false) => {
+    let ongoing = true
+    let roundWinner = undefined
+
+    let currCache = new Set()
+
+    while (ongoing) {
+        let currGameKey = createGameCacheKey(p1arr, p2arr)
+        let cp1 = p1arr.shift()
+        let cp2 = p2arr.shift()
+        if (currCache.has(currGameKey)) {
+            roundWinner = 'p1r' // if game array already happened, declare p1 as winenr
+            ongoing = false
+        } else {
+            currCache.add(currGameKey)
+
+            // winner's value should be above from the stacks
+
+            if (cp1 <= p1arr.length && cp2 <= p2arr.length) {
+                // recursive
+                roundWinner = recursiveBattle(_.take(_.cloneDeep(p1arr), cp1), _.take(_.cloneDeep(p2arr), cp2), false)
+                // console.log('entry', _.take(_.cloneDeep(p1arr), cp1).length, _.take(_.cloneDeep(p2arr), cp2).length)
+            } else if (cp1 > cp2) {
+                roundWinner = 'p1'
+            } else {
+                roundWinner = 'p2'
+            }
+        }
+
+        if (roundWinner === 'p1' || roundWinner === 'p1r') {
+            p1arr.push(cp1)
+            p1arr.push(cp2)
+        } else {
+            p2arr.push(cp2)
+            p2arr.push(cp1)
+        }
+
+        if (p1arr.length === 0) {
+            ongoing = false
+            roundWinner = 'p2'
+        } else if (p2arr.length === 0 || roundWinner === 'p1r') {
+            ongoing = false
+            roundWinner = 'p1'
+        }
+        // console.log(p1arr, p2arr, cp1, cp2, base, roundWinner)
+    }
+    return roundWinner
+}
+
+
 lineStream.on('line', (line) => {
     if (line === '') {
         return
@@ -50,6 +109,8 @@ lineStream.on('line', (line) => {
 
 
 lineStream.on('close', () => {
+
+    // part 1
     let ongoing = true
     let winner = []
 
@@ -64,7 +125,6 @@ lineStream.on('close', () => {
         } else {
             stacks.p2.push(cp2)
             stacks.p2.push(cp1)
-
         }
 
         if (stacks.p1.length === 0) {
@@ -79,73 +139,19 @@ lineStream.on('close', () => {
     for (let i = winner.length - 1; i >= 0; i--) {
         results[0] += (winner.length - i) * winner[i]
     }
-
-    let createGameCacheKey = (p1, p2) => {
-        return `p1~${p1.join('~')}~p2~${p2.join('~')}`
-    }
-/* 
-    ongoing = true
-    let globalGameCache = new Set()
-    while(ongoing){
-
-        let cp1 = p1.shift()
-        let cp2 = p2.shift()
-
-        
-        if (p1.length === 0) {
-            winner = p2
-            ongoing = false
-        }
-        
-        if (p2.length === 0) {
-            winner = p1
-            ongoing = false
-        }
-        
-        let c = createGameCacheKey(p1, p2)
-        if (globalGameCache.has(c)) {
-            winner = p1
-            ongoing = false
-        }
-        globalGameCache.add(c)
-
-    }
-
-    let recursiveBattle = (p1, p2) => {
-        if (p1.length === 0) {
-            winner = p2
-            return
-        }
-        if (p2.length === 0) {
-            winner = p1
-            return 'p2'
-        }
-
-        let cp1 = p1.shift()
-        let cp2 = p2.shift()
-        let pool = []
-        let gameCache = new Set()
-        let c = createGameCacheKey(p1, p2)
-        if (gameCache.has(c)) {
-            return 'p1'
-        }
-        gameCache.add(c)
-
-        pool.push(p1)
-        if (cp1 > cp2) {
-            pool.push(cp2)
-        } else {
-            pool.splice(0, 0, cp2)
-        }
-
-        if (cp1 <= p1.length || cp2 <= p2.length) {
-            // recursive battle, need to return winner
-        }
-    } */
-
     console.log(`Result #1: ${results[0]}`)
 
+    // part 2
+
     stacks = _.cloneDeep(baseStacks)
+    winner = stacks[recursiveBattle(stacks.p1, stacks.p2, true)]
+
+    // console.log(stacks.p1, stacks.p2)
+
+    for (let i = winner.length - 1; i >= 0; i--) {
+        results[1] += (winner.length - i) * winner[i]
+    }
+    console.log(`Result #2: ${results[1]}`)
 
     performance.mark('end')
     performance.measure('Start to end', 'start', 'end')
