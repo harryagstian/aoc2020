5275819b30d9f458d43e48753571b60515058d39
Harry Agustian <harryagustian777@yahoo.co.id>
2021-08-04 01:50:04 +0700

    Add some comments for day21

--- nodejs/day21/index.js
+++ nodejs/day21/index.js
@@ -14,8 +14,7 @@ const main = (runSample) => {
 
     // each lines guaranteed to have at least 1 (contains)
 
-    let allListedAllergens = []
-    let allListedIngredients = [] // 
+    let allListedIngredients = [] // list all occurence of ingredient
     const rawData = text.map(item => {
         const rules = /(.*)\(contains(.*)\)/g
         let [, i, a] = rules.exec(item) // all match, ingredients, allergens
@@ -23,12 +22,9 @@ const main = (runSample) => {
         i = i.trim().split(' ')
         a = a.trim().replace(/ /g, '').split(',')
 
-        allListedAllergens = allListedAllergens.concat(a)
         allListedIngredients = allListedIngredients.concat(i)
         return [i, a]
     })
-
-    // console.log(rawData)
 
     // for each listed allergen, one of the ingredients listed on same line is GUARANTEED to have the allergen
 
@@ -54,32 +50,30 @@ const main = (runSample) => {
 
     let stacks = [...guaranteedAllergens]
 
-    while (stacks.length > 0) {
+    while (stacks.length > 0) { // checks until no more item in stacks, meaning all allergens only have 1 ingredients
         const currentAllergen = stacks.shift()
         for (const [key, value] of Object.entries(ingredientsToAllergenMapping)) {
             if (value.length > 1) {
                 ingredientsToAllergenMapping[key] = _.remove(value, i => i !== currentAllergen)
-                if(ingredientsToAllergenMapping[key].length === 1){
+                if (ingredientsToAllergenMapping[key].length === 1) {
                     stacks.push(...ingredientsToAllergenMapping[key])
                     guaranteedAllergens.add(...ingredientsToAllergenMapping[key])
-                } 
+                }
             }
         }
     }
-    
+
     const results = [1, 0]
 
     // part 1
 
-    results[0] = _.without(allListedIngredients, ...guaranteedAllergens).length
+    results[0] = _.without(allListedIngredients, ...guaranteedAllergens).length // remove ingredients that guaranteed to have allergens from all ingredients occurence, count the length
 
     console.log(`Part 1 solution: ${results[0]}`)
 
     // part 2
 
-    const sortedAllergen = Object.keys(ingredientsToAllergenMapping).sort()
-
-    results[1] = sortedAllergen.map(key => ingredientsToAllergenMapping[key][0]).join(',')
+    results[1] = Object.keys(ingredientsToAllergenMapping).sort().map(key => ingredientsToAllergenMapping[key][0]).join(',') // sort by allergen, return corresponding ingredient name, join with comma
 
     console.log(`Part 2 solution: ${results[1]}`)
 
